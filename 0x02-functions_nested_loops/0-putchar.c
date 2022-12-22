aef80572215d28b519f5649c521b353bba9f0cf1
maya33 <jkafakoma@yahoo.com>
2022-12-22 00:33:57 -0800

    Test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include "main.h"
+#include "Jeremiah.h"
 /**
  * main - Entry point
  * Return: Always 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('J');
+	_putchar('e');
 	_putchar('r');
+	_putchar('e');
+	_putchar('m');
+	_putchar('i');
+	_putchar('a');
+	_putchar('h');
 	_putchar('\n');
 	return (0);
 }
