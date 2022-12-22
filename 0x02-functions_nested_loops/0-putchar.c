f142cb3e3bb42dbd6e47a9bbf8cb3753c5b5b108
maya33 <jkafakoma@yahoo.com>
2022-12-21 23:26:32 -0800

    Tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/*
+ * Title: 0-holberton.c
+ * Owner: Jeremiah Peter
+ *
+ */
+
+#include "jeremiah.h"
+
+/**
+ * main - Prints "Jeremiah" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('J');
+	_putchar('E');
+	_putchar('R');
+	_putchar('E');
+	_putchar('M');
+	_putchar('I');
+	_putchar('A');
+	_putchar('H');
+	_putchar('\n');
+
+	return (0);
+}
