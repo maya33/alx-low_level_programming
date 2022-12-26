14182d0b502482c7c46b5ade14736be9c0d2fcba
maya33 <jkafakoma@yahoo.com>
2022-12-26 01:46:25 -0800

    2 Alphabet letters

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,15 +9,13 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i < 10)
+	while (i <= 9)
 	{
-		c = 'a';
-		while (c <= 'z')
-		{
-			_putchar(c);
-			c++;
-		}
-		_putchar('\n');
-		i++;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
 	}
 }
