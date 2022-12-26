4b0921dd7187369667157955d91418181e670061
maya33 <jkafakoma@yahoo.com>
2022-12-26 01:12:18 -0800

    Printing letters

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,21 +2,22 @@
 /**
  * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
- * Return: x10 a-z
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
-	int n, co;
+	char c;
+	int i = 0;
 
-	co = 0;
-
-	while (co < 10)
+	while (i < 10)
 	{
-		for (n = 'a'; n <= 'z'; n++)
+		c = 'a';
+		while (c <= 'z')
 		{
-			_putchar(n);
+			_putchar(c);
+			c++;
 		}
-		co++;
 		_putchar('\n');
+		i++;
 	}
-}}
+}
