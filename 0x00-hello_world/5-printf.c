94326cde986c70ef8e6319d74e7ac57793b6f64d
maya33 <jkafakoma@yahoo.com>
2022-12-30 03:03:25 -0800

    Hello Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - prints 'with proper garmmar'
  * Return: Always 0 (Success)
  */
 
+
 int main(void)
+
 {
 	printf("With proper grammar, but the outcome is a piece of art, \n");
-	
 	return (0);
 }
