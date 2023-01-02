24e195c39545ee31a1641779baa46a17fca0e8d6
maya33 <jkafakoma@yahoo.com>
2023-01-02 08:51:12 -0800

    prints arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,13 +11,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
-		printf("%d", a[i]);
-		if (i == n - 1)
-		{
-			printf(",");
-			printf(" ");
-		}
-			
-	printf("\n");
+	for (i = 0; i < (n-1); i++
+			{
+			printf("%d, ", a[i]);
+			}
+			if (i == (n - 1));
+			{
+			printf("%d", a[n - i]);
+			}
+			printf("\n");
 }
+
