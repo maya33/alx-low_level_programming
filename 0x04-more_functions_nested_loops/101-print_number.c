22952390a51a94a0781b633b7d1197f7cf5257e1
maya33 <jkafakoma@yahoo.com>
2023-01-02 02:01:28 -0800

    Prints integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,21 @@
 #include "main.h"
-/**
- * print_number - a function that prints an integer
- * @n: input
- * Return: inputted integer
- */
-void print_number(int n)
-{
 
-	return (0);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
 }
