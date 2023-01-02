b3650391dde188eecb50a3dcc483dd1c14347852
maya33 <jkafakoma@yahoo.com>
2023-01-02 05:16:39 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,14 +6,14 @@
  * Return: Pointers
  *
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src)
 {
 	int index = 0, dest_len =0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index] && index < n; index++)
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
