d692ee5f05c325ad789b66e006861bb9d2c77dec
maya33 <jkafakoma@yahoo.com>
2023-01-03 06:03:14 -0800

    Linking strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,17 +6,15 @@
  * @src: copy from
  * Return: two concat strings
  */
-char *_strcat(char *dest, char *src)
+char *strcat(char *dest, const char *src)
 {
-	int i, n;
+	int index = 0, dest_len = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
+	while (dest[index++])
+		dest_len++;
 
-	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
-	{
-	}
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
