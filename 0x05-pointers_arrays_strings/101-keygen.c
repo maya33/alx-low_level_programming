a23d57c74a21d3823cdde24876fbf58f84efd888
maya33 <jkafakoma@yahoo.com>
2023-01-03 05:52:31 -0800

    Random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,26 +1,54 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - generates random valid passwords
- * Return: 0 (on success)
+ * main - Generates random valid passwords for the
+ *        
  *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int pass, sum;
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	$rand(time(NULL));
-	sum = 0;
-	while (sum <= 2685)
+	srand(time(0));
+
+	while (sum < 2772)
 	{
-		pass = (rand() % 126);
-		sum += pass;
-		printf("%c", pass);
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
-	printf("%c", 2772 -sum);
-	
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+
+	printf("%s", password);
+
 	return (0);
 }
-
