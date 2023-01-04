35d3bcbdef04e9796c515f13dc6ea01ee54ea9cb
maya33 <jkafakoma@yahoo.com>
2023-01-04 05:27:48 -0800

    Squareroot recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -23,12 +23,12 @@ int find_sqrt(int num, int root)
 }
 
 /**
- * squareroot - checks if perfect square
- * @n: input
- * @i: counter
- * Return: if square root
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
+ *
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
  */
-
 int _sqrt_recursion(int n)
 {
 	int root = 0;
