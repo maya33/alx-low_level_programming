9b7be7ada6e607e74de8dcc08af3de42ca5e3bc4
maya33 <jkafakoma@yahoo.com>
2023-03-31 00:41:56 -0700

    function to concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,16 +6,25 @@
  * @src: copy from
  * Return: two concat strings
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int count = 0, count2 = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (*(dest + count) != '\0*)
+	{
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+		count++;
+	}
 
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+
+		count++;
+		count2++;
+	}
 	return (dest);
 }
 
