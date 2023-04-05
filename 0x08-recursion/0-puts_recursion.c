63592913368ae514cc02de72b129b3ea19e62ab1
maya33 <jkafakoma@yahoo.com>
2023-04-05 02:33:31 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function like puts();
- * 
- * Return: Always 0 (Success)
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return
  */
 void _puts_recursion(char *s)
 {
