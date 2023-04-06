8a40b586533910487caa08a4409528a239914923
maya33 <jkafakoma@yahoo.com>
2023-04-06 02:11:55 -0700

    function to print 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main:function that  prints the name of the program
+ * @argc: number of command line argument counter
+ * @argv: argument vector
+ * Return: 1
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Working with Argument counters (argc) and argument vectors (argv) in project 0x0A-argc_argv
