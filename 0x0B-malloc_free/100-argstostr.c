54f1e71067d23787427608f5867742a6a1f89c4f
maya33 <jkafakoma@yahoo.com>
2023-04-11 14:13:05 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,46 +1,62 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * argstostr - function that concatenates all the arguments of your program
- *@ac: number of arguments
- *@av: arguments
- * Return: a pointer to a new string
+ * _strlen - returns the length of a string
+ * @s: string we find the length of
+ *
+ * Return: length of the string
  */
+
+int _strlen(char *s)
+{
+	int x = 0;
+
+	while (*(s + x) != '\0')
+		x++;
+	return (x);
+}
+
+/**
+ * argstostr - concatenates all the arguments of the program
+ * @ac: number of arguments
+ * @av: array of arguments
+ *
+ * Return: returns a pointer to the concatenated string
+ */
+
 char *argstostr(int ac, char **av)
 {
-	int i;
-	int j;
-	char *p = NULL;
-	int k;
-	int ext;
+	int x, y, z, length;
+	char *s;
 
-	k = 0;
-	ext = 0;
+	length = 1;
+	z = 0;
+
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
+
+	for (x = 0; x < ac; x++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			ext++;
-		}
+		length += _strlen(av[x]) + 1;
 	}
 
-	p = (char *)malloc(ext + ac + 1 * sizeof(char));
-	if (p == NULL)
+	s = malloc(sizeof(char) * length);
+	if (s == NULL)
+	{
 		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			p[k] = av[i][j];
-			k++;
-		}
-		p[k] = '\n';
-		k++;
 	}
-	p[k] = '\0';
-	return (p);
 
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; y < _strlen(av[x]); y++)
+		{
+			s[z] = av[x][y];
+			z++;
+		}
+		s[z] = '\n';
+		z++;
+	}
+	s[z] = '\0';
+	return (s);
+}
