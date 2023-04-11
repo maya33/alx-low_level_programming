1fc7b199e455b922dae44a1cab379fcc9d1befcf
maya33 <jkafakoma@yahoo.com>
2023-04-11 12:26:47 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s;
+
+    s = str_concat("Betty ", "Holberton");
+    if (s == NULL)
+    {
+        printf("failed\n");
+        return (1);
+    }
