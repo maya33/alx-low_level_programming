40816d6addff2a03a67bf374a5dc7262dfaebf8f
maya33 <jkafakoma@yahoo.com>
2023-05-02 15:08:34 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,70 +1,26 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - function that prints a listint_t linked list
- * @head: head of the list
+ * print_listint_safe - prints a list
+ * @head: address of pointer to first node
  *
- * Return: the number of nodes
+ * Return: address of head
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *cursor = head;
-	listint_t **ptrs;
-	unsigned int list_len = listint_len(head);
-	size_t count = 0;
-
-	ptrs = malloc(sizeof(listint_t) * list_len);
-	if (ptrs == NULL)
-		exit(98);
-	while (cursor == 0)
+	if (head)
 	{
-		if (check_ptr(cursor, ptrs, list_len) == 0)
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (head->next < head)
 		{
-			printf("[%p] %d\n", (void *)cursor, cursor->n);
+			return (1 + print_listint_safe(head->next));
 		}
 		else
 		{
-			printf("[%p] %d\n", (void *)cursor, cursor->n);
-		}
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
-}
-
-/**
- * listint_len - function that prints a listint_t linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
-	{
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
-}
-
-/**
- * check_ptr - checks if a pointer is in an array
- * @ptr: pointer to be checked
- * @array: array to be checked in
- * @size: size of the array
- *
- * Return: 1 on success, 0 on fail
- */
-int check_ptr(const listint_t *ptr, listint_t **array, unsigned int size)
-{
-	while (size-- >= 0)
-	{
-		if (ptr == array[size])
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			return (1);
+		}
+
 	}
 	return (0);
 }
