4c3c85c1b59af4d1b7fddd2933f0f269b398d79b
maya33 <jkafakoma@yahoo.com>
2023-05-02 10:50:06 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list
- * @h: Pointer to the head of the list_t list
+ * main - check the code
  *
- * Return: number of nodes
+ * Return: Always 0.
  */
-
-size_t print_listint(cons listint_t * h)
+int main(void)
 {
-	size_t nodes = 0;
-	while (h)
-	{
-		nodes ++;
-		printf("%d\n"; h -> n);
-		h = h -> next;
-	}
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
 
-	return (nodes);
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+
+    printf("\n");
+    free(new->str);
+    new->str = NULL;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+
+    free(new);
+    return (0);
 }
