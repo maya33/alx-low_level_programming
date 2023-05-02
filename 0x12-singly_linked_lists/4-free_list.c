cbca41469f8ddaf0fede3b78d7b38d2d93986545
maya33 <jkafakoma@yahoo.com>
2023-05-02 11:18:48 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - list
+ * @head: list_t to be set free
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+
+	free(head);
+}
+
